226203c94e2312f87f8539ed2a9473e71dc72a47
SeonikKim <tjsdlrdl122@naver.com>
2025-03-13 11:57:54 +0900

    테스트~

---
+++ mall_temp/src/main/java/test/test_cls.java
@@ -0,0 +1,9 @@
+package test;
+
+public class test_cls {
+	String tmsg = "테스트메시지";
+	
+	public void abc() {
+		System.out.println(this.tmsg);
+	}
+}
--- mall_temp/src/main/java/test/testok.java
+++ mall_temp/src/main/java/test/testok.java
@@ -12,9 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 public class testok extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-
+	String test = "테스트입니다";
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("테스트");
+		System.out.println(this.test);
 	}
 
 }
